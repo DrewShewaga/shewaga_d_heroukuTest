400421eb72c45a0a204d5b0c2355474092cf3836
DrewShewaga <cierenius@gmail.com>
2019-03-21 01:45:39 -0400

    dev.ds.beachChairs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Herouku App' });
+  res.render('index', { title: 'Beach Chairs' });
 });
 
 module.exports = router;
